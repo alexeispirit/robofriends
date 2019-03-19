e3c1a57257b9d37f50b5a31abc69d0888a4f5116
alexeispirit <alexeispirit@gmail.com>
2019-03-19 17:03:30 +0500

    Updates

---
+++ precache-manifest.66d07829903cc76704af82b1b0ab1339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "0954a6a9280292f8806e",
+    "url": "/robofriends/static/js/main.bf8f7f02.chunk.js"
+  },
+  {
+    "revision": "4827a17d8b07537ef3a8",
+    "url": "/robofriends/static/js/2.f03da986.chunk.js"
+  },
+  {
+    "revision": "0954a6a9280292f8806e",
+    "url": "/robofriends/static/css/main.bc9380f5.chunk.css"
+  },
+  {
+    "revision": "4827a17d8b07537ef3a8",
+    "url": "/robofriends/static/css/2.67f131bd.chunk.css"
+  },
+  {
+    "revision": "c1b43db81e7f79669236869d3ce5aa8e",
+    "url": "/robofriends/index.html"
+  }
+];
